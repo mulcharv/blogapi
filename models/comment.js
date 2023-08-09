8f5dd4f266152a08f2878a08d1084f30b78e9a17
Rahul Mulchandani <44975642+mulcharv@users.noreply.github.com>
2023-08-09 14:39:15 +0100

    update comment model

--- models/comment.js
+++ models/comment.js
@@ -8,7 +8,7 @@ const CommentSchema = new Schema({
     name: { type: Schema.Types.ObjectId, ref: "User", required: true },
     content: { type: String, required: true}, 
     post: { type: Schema.Types.ObjectId, ref: "Post", required: true},
-}, {timestamps: true });
+}, {timestamps: true, toObject: {virtuals: true}, toJSON: {virtuals: true} });
 
 CommentSchema.virtual("timestamp_formatted").get(function () {
     return DateTime.fromJSDate(this.createdAt).toLocaleString(DateTime.DATE_MED);
